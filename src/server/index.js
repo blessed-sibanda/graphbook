07c21fcf7ea56a0b02d7c9a6e546298b917b6999
Blessed Sibanda <blessedsibanda.me@gmail.com>
2022-08-06 03:31:26 +0200

    setup express 7 serve frontend with it

---
+++ src/server/index.js
@@ -0,0 +1,14 @@
+import path from "path";
+import express from "express";
+
+const app = express();
+const root = path.join(__dirname, "../../");
+
+app.use("/", express.static(path.join(root, "dist/client")));
+app.use("/uploads", express.static(path.join(root, "uploads")));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(root, "/dist/client/index.html"));
+});
+
+app.listen(8000, () => console.log("Listening on port 8000!"));
