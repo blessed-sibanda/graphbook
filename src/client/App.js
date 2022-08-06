c41deaa2b2cad29d0f7733c15a3b1f2a2ea00b84
Blessed Sibanda <blessedsibanda.me@gmail.com>
2022-08-06 02:36:12 +0200

    add styles and images

--- src/client/App.js
+++ src/client/App.js
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
+import "../../assets/css/styles.css";
+
+const initialPosts = [
+  {
+    id: 2,
+    text: "Lorem ipsum",
+    user: {
+      avatar: "/uploads/avatar1.png",
+      username: "Test User",
+    },
+  },
+  {
+    id: 1,
+    text: "Lorem ipsum",
+    user: {
+      avatar: "/uploads/avatar2.png",
+      username: "Test User 2",
+    },
+  },
+];
 
 const App = () => {
-  return <div>Hello World!</div>;
+  const [posts, setPosts] = useState(initialPosts);
+  return (
+    <div className="container">
+      <div className="feed">
+        {initialPosts.map((post, i) => (
+          <div key={post.id} className="post">
+            <div className="header">
+              <img src={post.user.avatar} alt="" />
+              <h2>{post.user.username}</h2>
+            </div>
+            <p className="content">{post.text}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default App;
