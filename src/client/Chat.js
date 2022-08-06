085c4935c67dfdde896e19b86f1caefba32ee5de
Blessed Sibanda <blessedsibanda.me@gmail.com>
2022-08-06 15:36:04 +0200

    send messages in a chat

--- src/client/Chat.js
+++ src/client/Chat.js
@@ -1,5 +1,5 @@
-import { gql, useQuery } from "@apollo/client";
-import React from "react";
+import { gql, useMutation, useQuery } from "@apollo/client";
+import React, { useState } from "react";
 
 const GET_CHAT = gql`
   query chat($chatId: Int!) {
@@ -21,11 +21,52 @@ const GET_CHAT = gql`
   }
 `;
 
+const ADD_MESSAGE = gql`
+  mutation addMessage($message: MessageInput!) {
+    addMessage(message: $message) {
+      id
+      text
+      user {
+        id
+      }
+    }
+  }
+`;
+
 const Chat = (props) => {
   const { chatId, closeChat } = props;
   const { loading, error, data } = useQuery(GET_CHAT, {
     variables: { chatId },
   });
+  const [text, setText] = useState("");
+  const [addMessage] = useMutation(ADD_MESSAGE, {
+    update(cache, { data: { addMessage } }) {
+      cache.modify({
+        id: cache.identify(data.chat),
+        fields: {
+          messages(existingMessages = []) {
+            const newMessageRef = cache.writeFragment({
+              data: addMessage,
+              fragment: gql`
+                fragment NewMessage on Chat {
+                  id
+                }
+              `,
+            });
+            return [...existingMessages, newMessageRef];
+          },
+        },
+      });
+    },
+  });
+
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter" && text.length) {
+      addMessage({ variables: { message: { text, chatId } } }).then(() =>
+        setText("")
+      );
+    }
+  };
   if (loading)
     return (
       <div className="chatWindow">
@@ -55,6 +96,14 @@ const Chat = (props) => {
             {message.text}
           </div>
         ))}
+      </div>
+      <div className="input">
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          onKeyDown={handleKeyPress}
+        />
       </div>
     </div>
   );
