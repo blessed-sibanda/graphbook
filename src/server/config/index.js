afec3faaf77439e82bea9e1677b246599fb593c6
Blessed Sibanda <blessedsibanda.me@gmail.com>
2022-08-06 06:10:00 +0200

    connect to mysql DB & create Post model

---
+++ src/server/config/index.js
@@ -0,0 +1,28 @@
+module.exports = {
+  development: {
+    username: "devuser",
+    password: "mysecret",
+    database: "graphbook_dev",
+    host: "localhost",
+    dialect: "mysql",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  },
+  production: {
+    username: process.env.username,
+    password: process.env.password,
+    database: process.env.database,
+    host: process.env.host,
+    dialect: "mysql",
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+  },
+};
