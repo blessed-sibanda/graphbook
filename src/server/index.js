9c4d82ebdd82bb4cfa61f6d217d95e33132493ce
Blessed Sibanda <blessedsibanda.me@gmail.com>
2022-08-06 04:58:45 +0200

    view & add posts via graphql

--- src/server/index.js
+++ src/server/index.js
@@ -3,25 +3,42 @@ import express from "express";
 import helmet from "helmet";
 import cors from "cors";
 import compress from "compression";
+import services from "./services";
 
 const app = express();
 
-app.use(helmet());
-app.use(
-  helmet.contentSecurityPolicy({
-    directives: {
-      defaultSrc: ["'self'"],
-      scriptSrc: ["'self'", "'unsafe-inline'"],
-      styleSrc: ["'self'", "'unsafe-inline'"],
-      imgSrc: ["'self'", "data:", "*.amazonaws.com"],
-    },
-  })
-);
-app.use(helmet.referrerPolicy({ policy: "same-origin" }));
+if (process.env.NODE_ENV === "production") {
+  app.use(helmet());
+  app.use(
+    helmet.contentSecurityPolicy({
+      directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'", "'unsafe-inline'"],
+        styleSrc: ["'self'", "'unsafe-inline'"],
+        imgSrc: ["'self'", "data:", "*.amazonaws.com"],
+      },
+    })
+  );
+  app.use(helmet.referrerPolicy({ policy: "same-origin" }));
+}
 
 app.use(compress());
 
 app.use(cors());
+
+const serviceNames = Object.keys(services);
+
+for (let i = 0; i < serviceNames.length; i++) {
+  const name = serviceNames[i];
+  if (name === "graphql") {
+    (async () => {
+      await services[name].start();
+      services[name].applyMiddleware({ app });
+    })();
+  } else {
+    app.use(`/${name}`, services[name]);
+  }
+}
 
 const root = path.join(__dirname, "../../");
 
---
+++ src/server/services/graphql/schema.js
@@ -0,0 +1,38 @@
+import { gql } from "apollo-server-express";
+
+const typeDefinitions = gql`
+  type User {
+    avatar: String
+    username: String
+  }
+
+  type Post {
+    id: Int
+    text: String
+    user: User
+  }
+
+  input PostInput {
+    text: String!
+  }
+
+  input UserInput {
+    username: String!
+    avatar: String!
+  }
+
+  type RootQuery {
+    posts: [Post]
+  }
+
+  type RootMutation {
+    addPost(post: PostInput!, user: UserInput!): Post
+  }
+
+  schema {
+    query: RootQuery
+    mutation: RootMutation
+  }
+`;
+
+export default [typeDefinitions];
