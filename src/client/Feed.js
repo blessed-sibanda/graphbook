9d0edc82f53c528dd5e5c561a6fb3432124f481e
Blessed Sibanda <blessedsibanda.me@gmail.com>
2022-08-06 19:28:13 +0200

    refactor out queries and mutations from feed component

--- src/client/Feed.js
+++ src/client/Feed.js
@@ -4,81 +4,15 @@ import InfiniteScroll from "react-infinite-scroll-component";
 import Loading from "./components/loading";
 import Error from "./components/error";
 import Post from "./components/post";
-
-const GET_POSTS = gql`
-  query postsFeed($page: Int, $limit: Int) {
-    postsFeed(page: $page, limit: $limit) {
-      posts {
-        id
-        text
-        user {
-          avatar
-          username
-        }
-      }
-    }
-  }
-`;
-
-const ADD_POST = gql`
-  mutation addPost($post: PostInput!) {
-    addPost(post: $post) {
-      id
-      text
-      user {
-        username
-        avatar
-      }
-    }
-  }
-`;
+import { GET_POSTS } from "./apollo/queries/getPosts";
+import { useAddPostMutation } from "./apollo/mutations/addPost";
 
 const Feed = () => {
   const [hasMore, setHasMore] = useState(true);
   const [page, setPage] = useState(0);
 
   const [postContent, setPostContent] = useState("");
-  const [addPost] = useMutation(ADD_POST, {
-    update(cache, { data: { addPost } }) {
-      // const data = cache.readQuery({ query: GET_POSTS });
-      // const newData = { posts: [addPost, ...data.posts] };
-      // cache.writeQuery({ query: GET_POSTS, data: newData });
-
-      cache.modify({
-        fields: {
-          postsFeed(existingPostsFeed) {
-            const { posts: existingPosts } = existingPostsFeed;
-            const newPostRef = cache.writeFragment({
-              data: addPost,
-              fragment: gql`
-                fragment NewPost on Post {
-                  id
-                }
-              `,
-            });
-
-            return {
-              ...existingPostsFeed,
-              posts: [newPostRef, ...existingPosts],
-            };
-          },
-        },
-      });
-    },
-    optimisticResponse: {
-      __typename: "mutation",
-      addPost: {
-        __typename: "Post",
-        text: postContent,
-        id: -1,
-        user: {
-          __typename: "User",
-          username: "Loading...",
-          avatar: "/uploads/loading.gif",
-        },
-      },
-    },
-  });
+  const [addPost] = useAddPostMutation(postContent);
 
   const handleSubmit = (event) => {
     event.preventDefault();
